c54f3ba9d6b118dad31d6d76520981e1be25e645
ericminio <eric.mignot@gmail.com>
2022-08-15 14:11:32 -0700

    more about commit

--- about/tcr.sh
+++ about/tcr.sh
@@ -6,15 +6,14 @@ source ../tcr.sh
 function test_commit_on_green {
     DIR=$(current_dir ${BASH_SOURCE[0]})
     cd $DIR/will-pass
-    rm sut.spec.js
     rm -rf .git
+    rm will-pass.spec.js
     git init
-    echo node_modules > .gitignore
     npm install
     git add .
     git status
     git commit -m init
-    touch sut.spec.js
+    touch will-pass.spec.js
     TTT_REPO="$DIR/will-pass"
     tcr
     committed=$(git log | grep ttt | tail -n 1 | wc -l)
