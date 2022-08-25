c9b33806a6f16c007bfbd3abd0ddb1185eeda7a1
ericminio <eric.mignot@gmail.com>
2022-08-25 15:08:42 -0700

    maybe

--- maybe.sh
+++ maybe.sh
@@ -19,25 +19,11 @@ function run() {
     esac
 }
 
-if [ $(rebaseConflicted) = "yes" ]; then
-    clearControlFiles
-    checkpoint
-    exit 0
-fi
-
-if [ $(rebaseSuccessed) = "yes" ]; then
+if [ $(shouldRun) = "yes" ]; then
     clearControlFiles
     run
-    checkpoint
-    exit 0
-fi
-
-if [ $(hasChecksumChanged) -eq 0 ]; then
-    echo "$TTT_RUN -> no change -> exit"
-    clearControlFiles
     checkpoint
 else
     clearControlFiles
-    run
     checkpoint
 fi
